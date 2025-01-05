f2ca12e60395b7f4a9b206434228405144087a36
elyseeelius <elyseeelius@outlook.com>
2025-01-05 14:30:25 -0500

    added our database models

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from models import (Base, session, Book, engine)
+
 # import models
 # main menu - add, search analysis, exit, view
 # add books to the database
@@ -7,3 +9,5 @@
 # data cleaning
 # loop runs program
 
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
