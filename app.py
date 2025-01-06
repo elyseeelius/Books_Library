d79dd554a4bdd8538eabb0797b5a1b94a2f9714e
elyseeelius <elyseeelius@outlook.com>
2025-01-06 05:39:08 -0500

    add an edit and delete functionality

--- app.py
+++ app.py
@@ -6,6 +6,23 @@ import datetime
 import time
 
 
+def submenu():
+    while True:
+        print("""
+                   \r1) Edit
+                   \r2) Delete
+                   \r3) Return to main menu""")
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input("""
+                           \rPlease choose one of the options above.
+                           \rA number from 1 - 3
+                           \rPress enter to try again
+                           """)
+
+
 
 def menu():
     while True:
@@ -28,13 +45,6 @@ def menu():
                     """)
 
 
-# import models
-# main menu - add, search analysis, exit, view
-# add books to the database
-# edit books
-# delete books
-# search books
-# data cleaning
 
 
 
@@ -100,6 +110,29 @@ def clean_id(id_str, options):
             return
 
 
+def edit_check(column_name, current_value):
+    print(f'\n **** EDIT {column_name} ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+        print(f'\rCurrent Value:{current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\r Current Value: {current_value}')
+
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to? ')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return  changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to? ')
+
 
 
 
@@ -157,7 +190,7 @@ def app():
             while id_error:
                 id_choice = input(f"""
                       \nId Options: {id_options}
-                      \rBood id: """)
+                      \rBook id: """)
                 id_choice = clean_id(id_choice,id_options)
                 if type(id_choice) == int:
                     id_error = False
@@ -165,10 +198,22 @@ def app():
             print(f"""
                     \n{the_book.title} by {the_book.author}
                     \rPublished {the_book.published_date}
-                    \rPrice: ${the_book.price / 100}
-                    
-                    """)
-            input("\nPress enter to return to the main menu")
+                    \rPrice: ${the_book.price / 100}""")
+            sub_choice = submenu()
+            if sub_choice == '1':
+                # edit
+                the_book.title = edit_check('Title',the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated!')
+                time.sleep(1.5)
+            elif sub_choice == '2':
+                session.delete(the_book)
+                session.commit()
+                print('Book deleted!')
+                time.sleep(1.5)
         elif choice  == '4':
             # analysis
             pass
